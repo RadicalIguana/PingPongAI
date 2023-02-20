c07cb8168275f14bcb27787a098766424f30255d
RadicalIguana <d.lapsar16@gmail.com>
2023-02-20 15:16:47 +0500

    Наработки списка друзей

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -31,9 +31,6 @@ export default function DefaultLayout() {
     <div>
         <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
           <Link className="link navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" to="/user">{user.name}</Link>
-          <button className="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
           <div className="navbar-nav"> 
             <div className="nav-item text-nowrap">
               <a className="nav-link px-3" href="#" onClick={onLogout}>Sign out</a>
@@ -52,7 +49,6 @@ export default function DefaultLayout() {
                       <Link className="nav-link active" to='/play'>Play a game</Link>
                   </li>
                   <li className="nav-item">
-                      <span data-feather="file" className="align-text-bottom"></span>
                       <Link className="nav-link active" to='/user'>Profile</Link>
                   </li>
                   <li className="nav-item">
