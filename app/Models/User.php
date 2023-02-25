c1291d8d3d9e0977276b95ae0b528fc5f679fc8d
RadicalIguana <d.lapsar16@gmail.com>
2023-02-25 20:18:20 +0500

    wip(FriendList): Добавлены модели и таблицы

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function users() {
+        return $this->belongToMany(User::class, 'users_friends', 'user_id', 'friend_id');
+    }
 }
--- react/src/views/Social.jsx
+++ react/src/views/Social.jsx
@@ -11,34 +11,14 @@ export default function Social() {
         <table className="table">
           <thead>
             <tr>
-              <th scope="col">#</th>
               <th scope="col">ID</th>
               <th scope="col">Name</th>
-              <th scope="col">Email</th>
-              <th scope="col">Actions</th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <th scope="row">1</th>
               <td>35</td>
               <td>Mark</td>
-              <td>Mark@example.com</td>
-              <td>View profile - Delete</td>
-            </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>28</td>
-              <td>NotMark</td>
-              <td>NotMark@example.com</td>
-              <td>View profile - Delete</td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
-              <td>122</td>
-              <td>AndMark</td>
-              <td>AndMark@example.com</td>
-              <td>View profile - Delete</td>
             </tr>
           </tbody>
         </table>
