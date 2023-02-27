0fb9f171c34b4e4d211c8684012556063a191fef
RadicalIguana <d.lapsar16@gmail.com>
2023-02-27 17:08:44 +0500

    workflow(frontend-api): Changed axios api to laravel

--- react/src/axios-client.jsx
+++ react/src/axios-client.jsx
@@ -1,10 +1,7 @@
 import axios from 'axios'
 
 const axiosClient = axios.create({
-    // baseURL: `http://pingpong.test/api`
     baseURL: `http://127.0.0.1:8000/api`,
-    // baseURL: `http://127.0.0.1:80/api`
-    
 })
 
 axiosClient.interceptors.request.use((config) => {
