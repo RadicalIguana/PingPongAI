dbf40a523ce4df1c79ee0c5c0a0f5d047e7ecfbe
RadicalIguana <d.lapsar16@gmail.com>
2023-03-03 22:41:04 +0500

    wip(Game-Socket): Implementation of websocket in progress

--- react/src/views/Game/Play.jsx
+++ react/src/views/Game/Play.jsx
@@ -1,96 +1,69 @@
-import { useEffect, useRef, Component } from 'react'
 import { ReactP5Wrapper } from "react-p5-wrapper";
 
-class Board {
+class Player {
     constructor(x, y) {
-        this.x = x
-        this.y = y
-        this.speed = 0
+      this.x = x
+      this.y = y
+      this.speed = 0
     }
 
-    display(p5) {
-        p5.fill(255)
-        p5.rect(this.x, this.y, 25, 70)
+    show(p5) {
+        p5.rectMode(p5.CENTER);
+        p5.rect(this.x, this.y, 20, 80)
+    }
+
+    move(dir) {
+      this.speed = dir
     }
 
     update(p5) {
-        this.y += this.speed
+      this.y += this.speed
 
-        this.y = p5.constrain(this.y, -150, 80)
+      this.y = p5.constrain(this.y, -111, 111)
     }
-
-    move(y) {
-        this.speed = y
-    }
-
 }
 
 function sketch(p5) {
     
-    let board_left
-    let board_right
-    let board
+    let player
 
     p5.setup = () =>{
-        p5.createCanvas(600, 300, p5.WEBGL)
-        board_left = new Board(-300, -35)
-        board_right = new Board(275, -35)
+        p5.createCanvas(750, 300, p5.WEBGL)
+
+        player = new Player(-360, 0)
 
     }
 
     p5.draw = () => {
-        p5.background(0)
-        p5.normalMaterial();
-        board_left.display(p5)
-        board_right.display(p5)
-        board_left.update(p5)
-        board_right.update(p5)
+      p5.background(0)
+
+      player.show(p5)
+
+      player.update(p5)
+
+    }
+
+    p5.mouseDragged = () => {
+      player.move(2)
     }
 
     p5.keyPressed = () => {
-        if (p5.keyCode === 38) {
-            board_left.move(-2.5)
-        } else if (p5.keyCode == 87) {
-            board_right.move(-2.5)
-        } else if (p5.keyCode === 40) {
-            board_left.move(2.5)
-        } else if (p5.keyCode == 83) {
-            board_right.move(2.5)
-        }
+      if (p5.keyCode == p5.UP_ARROW) {
+        player.move(-2)
+      } else if (p5.keyCode == p5.DOWN_ARROW) {
+        player.move(2)
+      }
     }
 
     p5.keyReleased = () => {
-        if (p5.keyCode === 38 || p5.keyCode === 40) {
-            board_left.move(0)
-        }
-
-        if (p5.keyCode === 87 || p5.keyCode === 83) {
-            board_right.move(0)
-        }
+      if (p5.keyCode == p5.UP_ARROW || p5.keyCode == p5.DOWN_ARROW) {
+        player.move(0)
+      }
     }
 }
 
 export default function Play() {
 
-    return <ReactP5Wrapper sketch={sketch} />;
-
-
-
-    // // create a reference to the container in which the p5 instance should place the canvas
-    // const p5ContainerRef = useRef();
-
-    // useEffect(() => {
-    //     // On component creation, instantiate a p5 object with the sketch and container reference 
-    //     const p5Instance = new p5(sketch, p5ContainerRef.current);
-
-    //     // On component destruction, delete the p5 instance
-    //     return () => {
-    //         p5Instance.remove();
-    //     }
-    // }, []);
-
-    // return (
-    //     <div className="App" ref={p5ContainerRef} />
-    // );
+    return <ReactP5Wrapper sketch={sketch} />
 
 }
