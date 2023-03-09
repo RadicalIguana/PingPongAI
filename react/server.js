a54a3165d84bfb329999a64a38a2696750b449ac
RadicalIguana <d.lapsar16@gmail.com>
2023-03-09 23:27:22 +0500

    wip(game-socket): Fixed collision bugs and added websocket's room

--- react/server.js
+++ react/server.js
@@ -1,95 +1,57 @@
-var players = [];
-var connections = [];
-let ball
-function Player(id,x,y,v,w,h,p) {
-   this.id = id;
-   this.x = x;
-   this.y = y;
-   this.w = w;
-   this.h = h;
-   this.points = p;
-}
 
-function Ball(id,x,y,xv,yv,r) {
-  this.id = id;
-  this.x = x;
-  this.y = y;
-  this.xv = xv;
-  this.yv = yv;
-  this.r = r;
-}
-
-
-var express = require('express'); 
-var app = express(); 
-var server = app.listen(3001, "192.168.43.180");
-console.log("Server listening on port 3001");
-
-
-var io = require('socket.io')(server, {
+const express = require('express')
+const app = express()
+const server= require('http').createServer(app);
+const io = require('socket.io')(server,{
     cors:{
-        origin: "*"
+        origin: '*',
     }
 })
 
 
-function getCounter(){
-	io.sockets.emit('getCounter',connections.length);
-	console.log(connections.length);
-}
-
-setInterval(heartbeat,33);
-
-function heartbeat(){
-	io.sockets.emit('heartbeat',players);
-}
+const PORT = 4000;
 
 
 
-setInterval(heartbeatBall,33);
 
-function heartbeatBall(){
-	io.sockets.emit('heartbeatBall', ball);
-}
-
-
-io.sockets.on('connection', (socket) => { 
-	connections.push(socket);
-	getCounter();
-	socket.on('start', (data) => {
-		let pl = new Player(socket.id,data.x,data.y,data.w,data.h,data.points);
-		players.push(pl);
-	}); 
-
-	socket.on('startBall', (data) => {
-		ball = new Ball(socket.id,data.x,data.y,data.xv,data.yv,data.r);
-	}); 
-
-	socket.on('disconnect', (data) => {
-		connections.splice(connections.indexOf(socket),1);
-		console.log("disconnected");
-	});
-
-	socket.on('update', (data) => {
-        let player
-		for(var i = 0; i < players.length; i++){
-			if(socket.id === players[i].id)
-				player = players[i];
-		}
-		player.x = data.x;
-		player.y = data.y;
-		player.v = data.v;
-		player.w = data.w;
-		player.h = data.h;
-		player.points = data.points;
-	}); 
-
-	socket.on('updateBall',function(data){
-		ball.x = data.x;
-		ball.y = data.y;
-		ball.xv = data.xv;
-		ball.yv = data.yv;
-		ball.r = data.r;
-	}); 
-
+server.listen(PORT, ()=>{
+    console.log(`Listening on ${PORT} ...`)
 });
+
+let readyPlayerCount = 0;
+
+const pongNameSpace = io.of('/pong')    
+
+io.sockets.on('connection', (socket) => {
+        let room;
+    
+        console.log('a user connected', socket.id);
+
+    socket.on('ready', ()=>{
+        room = 'room ' + Math.floor(readyPlayerCount/2);
+        socket.join(room);
+        console.log('Player ready ', socket.id,room);
+
+        readyPlayerCount++;
+        console.log(readyPlayerCount)
+
+        if (readyPlayerCount%2 ===0){
+            // broadcast
+            io.sockets.in(room).emit('startGame', socket.id)
+        }
+    })
+
+    socket.on('paddleMove', (paddleData)=>{
+        socket.to(room).emit('paddleMove', paddleData);
+    })
+
+    socket.on('ballMove',(ballData)=>{
+        socket.to(room).emit('ballMove',ballData);
+    });
+
+    socket.on('disconnect',(reason)=>{
+        console.log(`Client ${socket.id} disconnected: ${reason}`)
+        socket.leave(room)
+    })
+})
+
--- react/src/views/Game/Play.jsx
+++ react/src/views/Game/Play.jsx
@@ -1,211 +1,219 @@
 import { ReactP5Wrapper } from "react-p5-wrapper";
-import io from 'socket.io-client'
+import { useEffect, useRef } from 'react'
+import { io } from 'socket.io-client'
 
-function Player(p5, x) {
-  
-  this.x = x
-  this.y = p5.height / 2
-  this.velocity = 4
-  this.w = 20
-  this.h = 80
-  this.top = this.x - this.h/2
-  this.bottom = this.x + this.h/2
-  this.points = 0
-
-  this.show = (p5) => {
-    p5.fill(255)
-    p5.rectMode(p5.CENTER)
-    p5.rect(this.x, this.y, this.w, this.h)
-  }
-
-  this.move = (p5) => {
-    if (this.y < p5.mouseY) 
-      this.y += this.velocity
-    else if (this.y > p5.mouseY) 
-      this.y -= this.velocity
-  }
-}
-
-function Ball(p5, x, y, xv, yv, r) {
-  this.x = x
-  this.y = y
-  this.xv = xv
-  this.yv = yv
-  this.r = r
-
-  this.show = (p5) => {
-    p5.fill(255)
-    p5.ellipse(this.x, this.y, 15, 15)
-  }
-
-  this.move = (p5) => {
-    if (this.y < 1) {
-      this.yv = 5
-    } 
-    if (this.y >= p5.height) {
-      this.yv = -5
-    }
-    this.y += this.yv
-    this.x += this.xv
-
-  }
-
-  this.collision = function(p5, p){
-    var r = p5.floor(p5.random(2));
-    if(this.y <= p.y + p.h/2 && this.y >= p.y - p.h/2){
-      if (this.x >= p.top && this.x <= p.bottom){
-        if(r === 1)
-            if(this.y-p.y < 0)
-              this.yv = 5;
-            else if(this.y - p.y == 0)
-              this.yv = 0;
-            else
-              this.yv = -5;
-        return true;
-      }
-    } 
-    return false;
-  }
-}
-
-function sketch(p5) {
-  let socket
-  let p
-  let b
-  let players = []
-  let go = false
-  let counter = 0
-  
-  p5.setup = () => {
-    socket = io.connect('http://192.168.43.180:3001');
-    p5.createCanvas(750,500);
-    b = new Ball(p5.width/2, p5.height/2, 4, 4, 15);
-    socket.on('getCounter', (data) => {
-    counter = data;
-    if(p === undefined){
-      if(counter % 2 === 0 )
-        p = new Player(p5, 0);
-      else
-        p = new Player(p5, p5.width);
-    }
-    var data = {
-    x:p.x,
-    y:p.y,
-    v:p.velocity,
-    w:p.w,
-    h:p.h,
-    points:p.points
-  };
-  socket.emit('start', data);
-
-  var data = {
-    x:b.x,
-    y:b.y,
-    xv:b.xv,
-    yv:b.yv,
-    r:b.r
-  };
-  socket.emit('startBall', data);
-  
-  if(counter === 2){
-    go = true;
-  }
-  });
-      
-
-  socket.on('heartbeat', (data) => {
-    players = data;
-  });
-
-  socket.on('heartbeatBall', (data) => {
-    if(data !== null){
-      b.x = data.x,
-      b.y = data.y,
-      b.xv = data.xv,
-      b.yv = data.yv,
-      b.r = data.r
-  }
-  });
-
-}
-
-  p5.draw = () => {
-    p5.background(0);
-    p5.rect(p5.width/2,0,5,1000);
-    p5.fill(255)
-    p5.textSize(40);
-    if(go === false)
-      p5.text("Waiting for other player.", p5.width/2 - 200, p5.height/2);
-      p5.fill(255);
-    if(go === true){
-    for(var i = 0; i < players.length; i++){
-      var id = players[i].id;
-      if(id !== socket.id){
-        p5.fill(255,0,0);
-        p5.rectMode(p5.CENTER);
-        p5.rect(players[i].x,players[i].y,players[i].w,players[i].h);
-      }
-    }
-    showPoints(p5, players);
-    p.show(p5);
-    p.move(p5);
-    b.show(p5);
-    b.move(p5);
-    if(b.collision(p5, p) && p.x === 0)
-      b.xv = 4;
-    if(b.collision(p5, p) && p.x === p5.width)
-      b.xv = -4;
-    if(b.x < 0){
-      throwBall(p5, b);
-      if(p.x === p5.width)
-        p.points++;
-    }
-    if(b.x > p5.width){
-        throwBall(p5, b);
-        if(p.x === 0)
-          p.points++;
-    }
-    var data = {
-    x:p.x,
-    y:p.y,
-    w:p.w,
-    h:p.h,
-    points:p.points
-    };
-
-  socket.emit('update',data);
-
-  var data = {
-    x:b.x,
-    y:b.y,
-    xv:b.xv,
-    yv:b.yv,
-    r:b.r
-  };
-  socket.emit('updateBall',data);
-}}
-}
-
-function throwBall(p5, b) {
-  b.x = p5.width / 2
-  b.y = p5.height / 2
-}
-
-function showPoints(p5, p){
-  p5.textSize(80);
-  p5.fill(0, 102, 153);
-  for(var i = 0; i < p.length; i++){
-    if(p[i].points !== undefined){
-      if(p[i].x === 0)
-        p5.text(p[i].points.toString(), p5.width/2 - 100, p5.height-100);
-      else
-        p5.text(p[i].points.toString(), p5.width/2 + 100, p5.height-100);
-    }
-  }
-}
 
 export default function Play() {
 
-    return <ReactP5Wrapper sketch={sketch} />
+  useEffect(() => {
+
+    const canvas = document.getElementById('canvas')
+    const ctx = canvas.getContext('2d')
+    const socket = io.connect('http://localhost:4000')
+    let isReferee = false
+    let paddleIndex = 0
+
+    const width = 300
+    const height = 500
+
+    // Paddle
+    let paddleHeight = 10
+    let paddleWidth = 50
+    let paddleDif = 25
+    let paddleX = [ 135, 135 ]
+    let trajectoryX = [ 0, 0 ]
+    let playerMoved = false
+
+    // Ball
+    let ballX = 160
+    let ballY = 250
+    let ballRadius = 5
+    let ballDirection = 1
+
+    // Speed
+    let speedX = 0
+    let speedY = 2
+
+    // Score
+    let score = [ 0, 0 ]
+
+    function createCanvas() {
+      canvas.width = width
+      canvas.height = height
+      renderCanvas()
+    }
+
+    function renderIntro() {
+      ctx.fillStyle = 'black'
+      ctx.fillRect(0, 0, width, height)
+
+      ctx.fillStyle = 'white'
+      ctx.font = '32px Courier New'
+      ctx.fillText("Waiting for opponent...", 20, (canvas.height / 2) - 30)
+    }
+
+    function renderCanvas() {
+      ctx.fillStyle = 'black'
+      ctx.fillRect(0, 0, width, height)
+
+      ctx.fillStyle = 'white'
+
+      // Top paddle
+      ctx.fillRect(paddleX[1], 5, paddleWidth, paddleHeight)
+
+      // Bottom paddle
+      ctx.fillRect(paddleX[0], height - 15, paddleWidth, paddleHeight)
+    
+      // Dashed center line
+
+
+      // Ball
+      ctx.beginPath()
+      ctx.arc(ballX, ballY, ballRadius, 2 * Math.PI, false)
+      ctx.fillStyle = 'white'
+      ctx.fill()
+
+      // Score
+      ctx.font = "32px Courier New"
+      ctx.fillText(score[1], 20, (canvas.height / 2) + 50)
+      ctx.fillText(score[0], 20, (canvas.height / 2) - 30)
+    }
+
+    function ballMove() {
+      ballY += speedY * ballDirection
+      if (playerMoved) {
+        ballX += speedX
+      }
+      socket.emit('ballMove', {
+        ballX, 
+        ballY,
+        score
+      })
+    }
+
+    function ballReset() {
+      ballX = width / 2
+      ballY = height / 2
+      speedY = 3
+
+      socket.emit('ballMove', {
+        ballX,
+        ballY,
+        score
+      })
+    }
+
+    function ballBoundaries() {
+      // Bounce off Left Wall
+      if (ballX < 0 && speedX < 0) {
+        speedX = -speedX
+      }
+      // Bounce off Right Wall
+      if (ballX > width && speedX > 0) {
+        speedX = -speedX
+      }
+      // Bounce off player bottom paddle
+      if (ballY > height - paddleDif) {
+        if (ballX >= paddleX[0] && ballX <= paddleX[0] + paddleWidth) {
+          // Add speed on Hit
+          if (playerMoved) {
+            speedY += 1
+            // Max speed
+            if (speedX > 5) {
+              speedY = 5
+            }
+          }
+          ballDirection = -ballDirection
+          trajectoryX[0] = ballX - (paddleX[0] + paddleDif)
+          speedX = trajectoryX[0] * 0.3
+        } else {
+          // Reset Ball, add to Computer Score
+          ballReset()
+          score[1]++
+        }
+      }
+      // Bounce off computer paddle top
+      if (ballY < paddleDif) {
+        if (ballX >= paddleX[1] && ballX <= paddleX[1] + paddleWidth) {
+          // Add speed on hit
+          if (playerMoved) {
+            speedY += 1
+            // Max speed
+            if (speedY > 5) {
+              speedY = 5
+            }
+          }
+          ballDirection = -ballDirection
+          trajectoryX[1] = ballX - (paddleX[1] + paddleDif)
+          speedX = trajectoryX[1] * 0.3
+        } else {
+          ballReset()
+          score[0]++
+        }
+      }
+    }
+
+    function animate() {
+      if (isReferee) {
+        ballMove()
+        ballBoundaries()
+      }
+      renderCanvas()
+      window.requestAnimationFrame(animate)
+    }
+
+    function loadGame() {
+      createCanvas()
+      renderIntro()
+      socket.emit('ready')
+    }
+
+    function startGame() {
+      paddleIndex = isReferee ? 0 : 1
+      window.requestAnimationFrame(animate)
+      canvas.addEventListener('mousemove', (e) => {
+        playerMoved = true
+        paddleX[paddleIndex] = e.offsetX
+        if (paddleX[paddleIndex] < 0) {
+          paddleX[paddleIndex] = 0
+        }
+        if (paddleX[paddleIndex] > (width - paddleWidth)) {
+          paddleX[paddleIndex] = width - paddleWidth
+        }
+        socket.emit('paddleMove', {
+          xPosition: paddleX[paddleIndex]
+        })
+        canvas.style.cursor = 'none'
+      })
+    }
+
+    loadGame()
+
+    socket.on('connect', () => {
+      console.log(`Connected as ${socket.id}`);
+    })
+
+    socket.on('startGame', (refreeId) => {
+      console.log(`Refree is ${refreeId}`);
+
+      isReferee = socket.id === refreeId
+
+      startGame()
+    })
+
+    socket.on('paddleMove', (paddleData) => {
+      const opponentPaddleIndex = 1 - paddleIndex
+      paddleX[opponentPaddleIndex] = paddleData.xPosition
+    })
+
+    socket.on('ballMove', (ballData) => {
+      ({ ballX, ballY, score } = ballData)
+    })
+
+  }, [])
+
+  return (
+    <canvas id="canvas"/>
+  )
 
 }
